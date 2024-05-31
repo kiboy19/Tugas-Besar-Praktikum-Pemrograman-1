f0bbc23e08eddf2d92ce18aa767e0f398b3838cb
Muhamad Ariel Septiadi <51844832+kiboy19@users.noreply.github.com>
2024-05-31 15:46:19 +0700

    Pembuatan Kelas Manajemen MataKuliah

---
+++ ManajemenMataKuliah.java
@@ -0,0 +1,11 @@
+package tubes;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class ManajemenMataKuliah {
+    private List<MataKuliah> mataKuliahList;
+//MASUKAN FUNGSINYA DIBAWAH INI.
+
+//MASUKAN FUNGSINYA DIATAS INI.
+}

18b47a88f4089b73b200a97c51353d3802d244f3
Muhamad Ariel Septiadi <51844832+kiboy19@users.noreply.github.com>
2024-05-31 16:16:34 +0700

    Update peletakan fungsi

--- Main.java
+++ Main.java
@@ -4,6 +4,26 @@ import java.util.Scanner;
 
 public class Main {
 //Masukan Fungsinya Dibawah ini
+  
+/* Bagian Arley - 
+ Fungsi Letakan Disini
+Akhir Bagian Arley*/
+
+/* Bagian Bima
+  Fungsi Letakan Disini
+  Akhir Bagian Bima */
+
+/* Bagian Ariel
+  Fungsi Letakan Disini
+  Akhir Bagian Ariel */
+
+/* Bagian Azis
+  Fungsi Letakan Disini
+  Akhir Bagian Azis */
+
+/* Bagian Aldi
+  Fungsi Letakan Disini
+  Akhir Bagian Azis */
 
 //Masukan Fungsinya Diatas Ini
 }

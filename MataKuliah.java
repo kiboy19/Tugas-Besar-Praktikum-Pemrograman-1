bb1652f382d21828033bafebb0c6ccb56dc0a80e
abdul aziz <125464874+duls3345@users.noreply.github.com>
2024-05-31 15:43:16 +0700

    Pembuatan Kelas MataKuliah

---
+++ MataKuliah.java
@@ -0,0 +1,42 @@
+package tubes;
+
+public class MataKuliah {
+    private String kode;
+    private String nama;
+    private int sks;
+
+    public MataKuliah(String kode, String nama, int sks) {
+        this.kode = kode;
+        this.nama = nama;
+        this.sks = sks;
+    }
+
+    public String getKode() {
+        return kode;
+    }
+
+    public void setKode(String kode) {
+        this.kode = kode;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public int getSks() {
+        return sks;
+    }
+
+    public void setSks(int sks) {
+        this.sks = sks;
+    }
+
+    @Override
+    public String toString() {
+        return "Kode: " + kode + ", Nama: " + nama + ", SKS: " + sks;
+    }
+}

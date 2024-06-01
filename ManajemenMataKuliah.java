a4f97918ff7c77f3a77e705f274b6d6c6972be17
Muhamad Ariel Septiadi <51844832+kiboy19@users.noreply.github.com>
2024-06-01 12:41:14 +0700

    Update ManajemenMataKuliah.java

--- ManajemenMataKuliah.java
+++ ManajemenMataKuliah.java
@@ -18,5 +18,33 @@ public void addMatakuliahHead(MataKuliah mataKuliah) {
 public void addMataKuliahTail(MataKuliah mataKuliah) {
         mataKuliahList.add(mataKuliah);
     }
+
+    /* Bagian Bima
+  Fungsi Letakan Disini
+  Akhir Bagian Bima */
+
+
+    public void removeMataKuliahTail() {
+        if (!mataKuliahList.isEmpty()) {
+            mataKuliahList.remove(mataKuliahList.size() - 1);
+        } else {
+            System.out.println("Daftar mata kuliah kosong");
+        }
+    }
+
+    public void removeMataKuliahMid(int index) {
+        if (index >= 0 && index < mataKuliahList.size()) {
+            mataKuliahList.remove(index);
+        } else {
+            System.out.println("Indeks di luar jangkauan");
+        }
+    }
+/* Bagian Azis
+  Fungsi Letakan Disini
+  Akhir Bagian Azis */
+
+/* Bagian Aldi
+  Fungsi Letakan Disini
+  Akhir Bagian Azis */
 //MASUKAN FUNGSINYA DIATAS INI.
 }

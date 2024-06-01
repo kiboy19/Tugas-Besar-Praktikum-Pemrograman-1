173437c195bca2aeadb35eca05c56e4bd70b6f57
Suep Khalifa <51029717+ArleyPrajaya@users.noreply.github.com>
2024-06-01 09:23:53 +0700

    Update ManajemenMataKuliah.java
    Penambahan tambahKuliah (head)
    Penambahan mataKuliah (Tail)

--- ManajemenMataKuliah.java
+++ ManajemenMataKuliah.java
@@ -6,6 +6,17 @@ import java.util.List;
 public class ManajemenMataKuliah {
     private List<MataKuliah> mataKuliahList;
 //MASUKAN FUNGSINYA DIBAWAH INI.
+ 
+public ManajemenMatakuliah() {
+        mataKuliahList = new LinkedList<>();
+    }
 
+public void addMatakuliahHead(MataKuliah mataKuliah) {
+        mataKuliahList.add(0, mataKuliah);
+    }
+
+public void addMataKuliahTail(MataKuliah mataKuliah) {
+        mataKuliahList.add(mataKuliah);
+    }
 //MASUKAN FUNGSINYA DIATAS INI.
 }
